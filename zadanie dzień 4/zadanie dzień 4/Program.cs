1816d078fbb0254f015bfe0502ee877a10ad7b92
git --version <aiand@wp.pl>
2024-03-05 18:04:45 +0100

    zadanie dzień 4

---
+++ zadanie dzień 4/zadanie dzień 4/Program.cs
@@ -0,0 +1,26 @@
+﻿var name = "Ewa";
+char sex = 'K';
+char sex2 = 'M';
+int age = 30;
+int age2 = 18;
+
+if (sex == 'K' && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30lat");
+}
+
+else if (name == "Ewa" && age == 30)
+{
+    Console.WriteLine("Ewa , lat 30");
+}
+
+else if (sex2 == 'M' && age2 < 18)
+{
+    Console.WriteLine("Nie pełnoletni mężczyzna");
+}
+
+
+else
+{
+    Console.WriteLine("Dane nie spełniają żadnego z warunków");
+}
